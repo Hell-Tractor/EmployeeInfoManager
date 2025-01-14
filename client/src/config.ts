960619be02e1a043bf4036963abc7c6ea26f5bd1
Hell-Tractor <1104840448@qq.com>
2025-01-14 17:13:34 +0800

    replace ip addr with domain name

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,3 @@
-export const SERVER: string = 'http://39.106.42.237:50002/';
-export const CLIENT: string = 'http://39.106.42.237/#/';
+export const SERVER: string = 'http://3wai.com.cn:50002/';
+export const CLIENT: string = 'http://3wai.com.cn/#/';
 export const QRCODE_SIZE: number = 200;
